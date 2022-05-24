b62fb9e5c53ab1630c4dc3a33143eecfa41f10ea
Julieta Ponti <jponti@fi.uba.ar>
2022-05-24 16:46:53 -0300

    casi listo

--- js/validacion.js
+++ js/validacion.js
@@ -6,78 +6,73 @@ const inputs = document.querySelectorAll('#formulario input');
 const expresiones = {
     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
     apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-    mail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    mail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
 
-formulario.addEventListener("submit", (evento) => {
-    evento.preventDefault();
-});
+const campos = {
+    nombre: false,
+    apellido: false,
+    mail: false
+}
+
+
 
 const validar_inputs = (e) => {
     switch (e.target.name) {
         case "nombre":
-            validar_nombre(expresiones.nombre, e.target);
+            validarCampo(expresiones.nombre, e.target, 'nombre');
             break;
         case "apellido":
-            validar_apellido(expresiones.apellido, e.target);
-            break;
-        case "edad":
-            validar_edad(expresiones.edad, e.target);
+            validarCampo(expresiones.apellido, e.target, 'apellido');
             break;
         case "mail":
-            validar_mail(expresiones.mail, e.target);
+            validarCampo(expresiones.mail, e.target, 'mail');
             break;
     }
 
 }
-
-const validar_nombre = (expresion, input_nombre) => {
-
-    if (expresion.test(input_nombre)) {
-        document.getElementById(`grupo__nombre`).classList.remove('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__nombre`).classList.add('formulario__grupo-correcto');
-        document.querySelector(`#grupo__nombre i`).classList.add('fa-check-circle');
-        document.querySelector(`#grupo__nombre i`).classList.remove('fa-times-circle');
-        document.querySelector(`#grupo__nombre .formulario__input-error`).classList.remove('formulario__input-error-activo');
-        campos[nombre] = true;
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos[campo] = true;
     } else {
-        document.getElementById(`grupo__nombre`).classList.add('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__nombre`).classList.remove('formulario__grupo-correcto');
-        document.querySelector(`#grupo__nombre i`).classList.add('fa-times-circle');
-        document.querySelector(`#grupo__nombre i`).classList.remove('fa-check-circle');
-        document.querySelector(`#grupo__nombre .formulario__input-error`).classList.add('formulario__input-error-activo');
-        campos[nombre] = false;
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos[campo] = false;
     }
-}
-
-const validar_apellido = (expresion, input_apellido) => {
-
-    if (expresion.test(input_apellido)) {
-        document.getElementById(`grupo__apellido`).classList.remove('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__apellido`).classList.add('formulario__grupo-correcto');
-        document.querySelector(`#grupo__apellido i`).classList.add('fa-check-circle');
-        document.querySelector(`#grupo__apellido i`).classList.remove('fa-times-circle');
-        document.querySelector(`#grupo__apellido .formulario__input-error`).classList.remove('formulario__input-error-activo');
-        campos[apellido] = true;
-    } else {
-        document.getElementById(`grupo__nombre`).classList.add('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__nombre`).classList.remove('formulario__grupo-correcto');
-        document.querySelector(`#grupo__nombre i`).classList.add('fa-times-circle');
-        document.querySelector(`#grupo__nombre i`).classList.remove('fa-check-circle');
-        document.querySelector(`#grupo__nombre .formulario__input-error`).classList.add('formulario__input-error-activo');
-        campos[nombre] = false;
-    }
-}
-
-const validar_edad = (expresiones.edad, e.target) => {
-
-}
-
-const validar_mail = (expresiones.mail, e.target) => {
-
 }
 
 inputs.forEach((input) => {
     input.addEventListener("keyup", validar_inputs);
     input.addEventListener("blur", validar_inputs);
+});
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    if (campos.nombre && campos.apellido && campos.mail) {
+        document.querySelectorAll(".formulario__grupo-correcto").forEach((icono) => {
+            icono.classList.remove("formulario__grupo-correcto");
+        });
+        document.getElementById("formulario__mensaje-exito").classList.add("formulario__mensaje-exito-activo");
+        setTimeout(() => {
+            document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+        }, 3000);
+        formulario.reset();
+        campos["nombre"] = false;
+        campos["apellido"] = false;
+        campos["mail"] = false;
+    } else {
+        document.getElementById("formulario__mensaje").classList.add("formulario__mensaje-activo");
+        setTimeout(() => {
+            document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+        }, 3000);
+    }
+
 });
